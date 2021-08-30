9dc250f69d61c95e20b64f237946cc38ca619c75
rockscripts <rockscripts@gmail.com>
2021-08-30 18:36:36 -0500

    last version 12.0

--- dian_efact/dian_efact/models/ir_sequence.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models, _
-from odoo.exceptions import Warning
-from odoo.osv import osv
-
-class ir_sequence(models.Model):
-    _inherit = 'ir.sequence'
-
--- dian_efact/dian_efact/models/uom_uom.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models, _
-
-class Uom(models.Model):
-    
-    _inherit = 'uom.uom'
-
-    dian_unit_code = fields.Char("Dian / Unidad de medida", name="dian_unit_code", default='')   
